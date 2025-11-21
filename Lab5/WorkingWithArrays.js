a399cd54e2e7e201296e9be3e01edd80a2f9e27a
Nisha Murthy D <nishamurthy22@gmail.com>
2025-11-21 15:30:12 -0500

    Fixing

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -50,7 +50,7 @@ export default function WorkingWithArrays(app) {
     todos.splice(todoIndex, 1);
     res.json(todos);
   };
-  
+
  const deleteTodo = (req, res) => {
     const { id } = req.params;
     const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
